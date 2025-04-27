bc6e3b3721797223accedc053e94b317ad050c9f
Martin Hild <martin@nimohi.de>
2025-04-27 16:16:55 +0200

    Initial commit

---
+++ arduino_plant_watering.ino
@@ -0,0 +1,18 @@
+// TEST ohne Sensor, ob Board und Pumpe funktionieren
+
+int Pumppin = A3; // Steuerung der Pumpe
+
+void setup()
+{
+  
+  pinMode(Pumppin, OUTPUT);//Ausgangspin für Relais
+  pinMode(A0, INPUT); // Eingangspin vom Bodensensor
+}
+
+void loop()
+{
+      digitalWrite(Pumppin, HIGH); // Motor Start
+      delay(2000);
+      digitalWrite(Pumppin, LOW); // Motor Stop
+      delay(3000);
+}
